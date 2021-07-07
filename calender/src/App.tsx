dd3fc1e895c440e37787b19f072833d05a6f20b5
Toshiki Shigihara <38338116+Shigi-p@users.noreply.github.com>
2021-07-07 13:02:15 +0900

    [add] 既存のプロジェクトファイルを削除、新しいプロジェクトファイルを追加 (#2)
    * [delete] 不必要そうだったので以前のプロジェクトファイルを削除
    
    * [add] node-version : 15系を使用するように指定(とりあえず)
    
    * [add] 新しくプロジェクトファイルを追加
    
    * [add] index.scss : sanitize.css追加
    
    * [style] App.tsx : 記述ちょっと修正
    
    * [update] README.md : 更新

---
+++ calender/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>
+        New hooks and typescript project!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
