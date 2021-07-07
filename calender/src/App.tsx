14add20144b4a4e0316f8db3b0fffa6c49a5d310
Shigi_p <shigi.522@gmail.com>
2021-07-07 12:56:50 +0900

    [add] 新しくプロジェクトファイルを追加

---
+++ calender/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        New hooks and typescript project!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
